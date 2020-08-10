8ca3cf6ebe8110efd194c49aaea053c07e76c96c
Casey77777 <35641498+Casey77777@users.noreply.github.com>
2020-08-10 02:22:57 -0700

    added 'test' function to test the bot's message capabilities

--- bot.py
+++ bot.py
@@ -3,13 +3,28 @@ import os
 import discord
 from dotenv import load_dotenv
 
+from discord.ext import commands
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!\n')
+    print(bot.guilds)
 
-client.run(TOKEN)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    else:
+        await bot.process_commands(message)
+
+@bot.command(name='test')
+async def test(ctx, *args):
+    await ctx.send('Your message was: ' + ' '.join(args))
+
+bot.run(TOKEN)
